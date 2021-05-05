8760dc6454f404addb56cd86c13948ebfd9004db
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-05-05 16:38:08 +0300

    calculator

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import './js/zanyatie-s-mentorom-1.js';
+// import './js/zanyatie-s-mentorom-1.js';
 // import './js/testModul-3-dz.js';
 // import './js/testModul-3.js';
-import './js/testModul-4-dz.js';
+// import './js/testModul-4-dz.js';
+// import './js/testModul-5-dz.js';
+import './js/testModul-6-dz.js';
 // import './js/callback.js';
