5f62448dbb1da6a036d6c69aad88777965056956
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-05-05 20:28:56 +0300

    calculator js - mentor

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 // import './js/testModul-3.js';
 // import './js/testModul-4-dz.js';
 // import './js/testModul-5-dz.js';
-import './js/testModul-6-dz.js';
+// import './js/testModul-6-dz.js';
 // import './js/callback.js';
+import './js/calculator.js';
